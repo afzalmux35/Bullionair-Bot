129592d6a5be102828731ec990c2e7e00e02215e
nexotech813 <nexotech813@gmail.com>
2025-12-16 02:23:35 +0500

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-2185229754-5b692",
-  "appId": "1:872571014730:web:aa91f023f259d2374220b3",
-  "apiKey": "AIzaSyCvI2yX51W2zw3ZPIED4P_e0U-nkMBv2Do",
-  "authDomain": "studio-2185229754-5b692.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "872571014730"
+  apiKey: "AIzaSyCvI2yX51W2zw3ZPIED4P_e0U-nkMBv2Do",
+  authDomain: "studio-2185229754-5b692.firebaseapp.com",
+  projectId: "studio-2185229754-5b692",
+  storageBucket: "studio-2185229754-5b692.firebasestorage.app",
+  messagingSenderId: "872571014730",
+  appId: "1:872571014730:web:aa91f023f259d2374220b3"
 };
